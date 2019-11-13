1bfac897535b5853f83a2413984ec758a19dbc18
zhengsizuo <42198964+zhengsizuo@users.noreply.github.com>
2019-11-13 20:49:57 +0800

    Add files via upload

---
+++ Deep_Q_Network.py
@@ -0,0 +1,120 @@
+import gym
+import random
+import torch
+import numpy as np
+from collections import deque
+from dqn_agent import Agent
+import matplotlib.pyplot as plt
+import cv2
+import time
+
+
+env = gym.make('Breakout-v0')
+# env.seed(0)
+state_size = env.observation_space.shape
+action_size = env.action_space.n
+print('Original state shape: ', state_size)
+print('Number of actions: ', env.action_space.n)
+
+agent = Agent((32, 4, 84, 84), action_size, seed=1)
+TRAIN = False
+
+
+def pre_process(observation):
+    x_t = cv2.cvtColor(cv2.resize(observation, (84, 84)), cv2.COLOR_BGR2GRAY)
+    ret, x_t = cv2.threshold(x_t, 1, 255, cv2.THRESH_BINARY)
+    return x_t
+
+
+def init_state(processed_obs):
+    return np.stack((processed_obs, processed_obs, processed_obs, processed_obs), axis=0)
+
+
+def dqn(n_episodes=30000, max_t=40000, eps_start=1.0, eps_end=0.01, eps_decay=0.9995):
+    """Deep Q-Learning.
+
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+    scores = []  # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start  # initialize epsilon
+    for i_episode in range(1, n_episodes + 1):
+        obs = env.reset()
+        obs = pre_process(obs)
+        state = init_state(obs)
+
+        score = 0
+        for t in range(max_t):
+            action = agent.act(state, eps)
+            next_state, reward, done, _ = env.step(action)
+
+            next_state = np.stack((state[1], state[2], state[3], pre_process(next_state)), axis=0)
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done:
+                break
+        scores_window.append(score)  # save most recent score
+        scores.append(score)  # save most recent score
+        eps = max(eps_end, eps_decay * eps)  # decrease epsilon
+        print('\tEpsilon now : {:.2f}'.format(eps))
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 1000 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+            print('\rEpisode {}\tThe length of replay buffer now: {}'.format(i_episode, len(agent.memory)))
+
+        if np.mean(scores_window) >= 50.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode - 100,
+                                                                                         np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(), 'dqn_checkpoint_solved.pth')
+            break
+
+    torch.save(agent.qnetwork_local.state_dict(), 'dqn_checkpoint_8.pth')
+    return scores
+
+
+if __name__ == '__main__':
+    if TRAIN:
+        start_time = time.time()
+        scores = dqn()
+        print('COST: {} min'.format((time.time() - start_time)/60))
+        print("Max score:", np.max(scores))
+
+        # plot the scores
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        plt.plot(np.arange(len(scores)), scores)
+        plt.ylabel('Score')
+        plt.xlabel('Episode #')
+        plt.show()
+
+    else:
+        # load the weights from file
+        agent.qnetwork_local.load_state_dict(torch.load('dqn_checkpoint_8.pth'))
+        rewards = []
+        for i in range(10):  # episodes
+            total_reward = 0
+            obs = env.reset()
+            obs = pre_process(obs)
+            state = init_state(obs)
+            for j in range(10000):  # frames
+                action = agent.act(state)
+                env.render()
+                next_state, reward, done, _ = env.step(action)
+                state = np.stack((state[1], state[2], state[3], pre_process(next_state)), axis=0)
+                total_reward += reward
+
+                # time.sleep(0.01)
+                if done:
+                    rewards.append(total_reward)
+                    break
+
+        print("Test rewards are:", *rewards)
+        print("Average rewards:", np.mean(rewards))
+        env.close()
---
+++ q_model.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import cv2
+import gym
+import matplotlib.pyplot as plt
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        "*** YOUR CODE HERE ***"
+        self.conv = nn.Sequential(
+            nn.Conv2d(state_size[1], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+            )
+
+        self.fc = nn.Sequential(
+            nn.Linear(64*7*7, 512),
+            nn.ReLU(),
+            nn.Linear(512, action_size)
+        )
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        conv_out = self.conv(state).view(state.size()[0], -1)
+        return self.fc(conv_out)
+
+
+def pre_process(observation):
+    x_t = cv2.cvtColor(cv2.resize(observation, (84, 84)), cv2.COLOR_BGR2GRAY)
+    ret, x_t = cv2.threshold(x_t, 1, 255, cv2.THRESH_BINARY)
+    return np.reshape(x_t, (1, 84, 84)), x_t
+
+
+def stack_state(processed_obs):
+    return np.stack((processed_obs, processed_obs, processed_obs, processed_obs), axis=0)
+
+
+if __name__ == '__main__':
+    env = gym.make('Breakout-v0')
+    print('State shape: ', env.observation_space.shape)
+    print('Number of actions: ', env.action_space.n)
+
+    obs = env.reset()
+    x_t, img = pre_process(obs)
+    state = stack_state(img)
+    print(np.shape(state[0]))
+
+    # plt.imshow(img, cmap='gray')
+    # 用cv2模块显示
+    # cv2.imshow('Breakout', img)
+    # cv2.waitKey(0)
+
+    state = torch.randn(32, 4, 84, 84)  # (batch_size, color_channel, img_height,img_width)
+    state_size = state.size()
+
+    cnn_model = QNetwork(state_size, action_size=4, seed=1)
+    outputs = cnn_model(state)
+    print(outputs)
